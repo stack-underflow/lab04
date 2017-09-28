745b267a6b72cbe8c1a5f2d02fe4b839b283b037
Underrateddev <rajyudhvir29@gmail.com>
2017-09-28 14:26:01 -0700

    fixed issue #9

---
+++ application/controllers/Delta/Force.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends Application
+{
+
+	function __construct()
+	{
+		parent::__construct();
+    }
+    
+	public function index()
+	{
+
+        $this->show (4);
+        
+	}
+
+}
